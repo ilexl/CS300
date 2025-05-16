98450f0e3117b4138ed21891b5a756e0ebae7da1
Alex <109491531+ilexl@users.noreply.github.com>
2025-05-16 12:05:22 +1200

    null reference fixed

--- Tank Game/Assets/Scripts/AlwaysLookAtCamera.cs
+++ Tank Game/Assets/Scripts/AlwaysLookAtCamera.cs
@@ -6,6 +6,7 @@ public class AlwaysLookAtCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Camera.main is null) { return; }
         transform.rotation = Camera.main.transform.rotation;
     }
 }
