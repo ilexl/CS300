f165e772650f5cec8d4e6cc6a71acfc2b3f78358
Gxaps <williamtetzlaff123@gmail.com>
2025-05-20 22:04:01 +1200

    ProjectileDatabase with some baseline test projectiles

--- Tank Game/Assets/Scripts/ProjectileDatabase.cs
+++ Tank Game/Assets/Scripts/ProjectileDatabase.cs
@@ -1,7 +1,40 @@
-namespace DefaultNamespace
+using System.Collections.Generic;
+
+public enum ProjectileKey
 {
-    public class ProjectileDatabase
+    M62,
+    M829A4
+}
+
+public class ProjectileDefinition
+{
+    public string Name { get; set; }
+    public MaterialKey MaterialKey { get; set; }
+    public Material Material => MaterialDatabase.GetMaterial(MaterialKey);
+    public float DiameterMm { get; set; }
+    public float LengthMm { get; set; }
+    public float VelocityMs { get; set; }
+
+    public ProjectileDefinition(string name, MaterialKey materialKey, float diameterMm, float lengthMm,
+        float velocityMs)
     {
-        
+        Name = name;
+        MaterialKey = materialKey;
+        DiameterMm = diameterMm;
+        LengthMm = lengthMm;
+        VelocityMs = velocityMs;
+    }
+}
+public static class ProjectileDatabase
+{
+    private static readonly Dictionary<ProjectileKey, ProjectileDefinition> ProjectileDefinition = new()
+    {
+        { ProjectileKey.M62 , new ProjectileDefinition("M62 (US)", MaterialKey.HighCarbonSteel, 76.2f, 355, 792)},
+        { ProjectileKey.M829A4 , new ProjectileDefinition("M829A4 (US)", MaterialKey.DepletedUranium, 25f, 850, 1570)}
+    };
+
+    public static ProjectileDefinition GetProjectile(ProjectileKey key)
+    {
+        return ProjectileDefinition[key];
     }
 }
