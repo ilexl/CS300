5a7b78f2f63d0ef72c84edff82f99d76a46e764c
Gxaps <williamtetzlaff123@gmail.com>
2025-05-20 21:11:50 +1200

    Update sphere generation

--- Tank Game/Assets/Scripts/DebugUtils.cs
+++ Tank Game/Assets/Scripts/DebugUtils.cs
@@ -1,18 +1,8 @@
 using UnityEngine;
 
-public class DebugUtils : MonoBehaviour
+public static class DebugUtils
 {
-    public Vector3 center = Vector3.zero;
-    public float radius = 5f;
-    public int segments = 36;
-    public Color color = Color.green;
-
-    void Update()
-    {
-        DrawSphere(center, radius, color, segments);
-    }
-
-    void DrawSphere(Vector3 position, float radius, Color color, int segments)
+    public static void DebugDrawSphere(Vector3 center, float radius, Color color, float duration = 0, int segments = 24)
     {
         float angleStep = 360f / segments;
 
@@ -22,9 +12,9 @@ public class DebugUtils : MonoBehaviour
             float angleA = angleStep * i;
             float angleB = angleStep * (i + 1);
 
-            Vector3 pointA = position + new Vector3(Mathf.Cos(Mathf.Deg2Rad * angleA) * radius, Mathf.Sin(Mathf.Deg2Rad * angleA) * radius, 0);
-            Vector3 pointB = position + new Vector3(Mathf.Cos(Mathf.Deg2Rad * angleB) * radius, Mathf.Sin(Mathf.Deg2Rad * angleB) * radius, 0);
-            Debug.DrawLine(pointA, pointB, color);
+            Vector3 pointA = center + new Vector3(Mathf.Cos(Mathf.Deg2Rad * angleA) * radius, Mathf.Sin(Mathf.Deg2Rad * angleA) * radius, 0);
+            Vector3 pointB = center + new Vector3(Mathf.Cos(Mathf.Deg2Rad * angleB) * radius, Mathf.Sin(Mathf.Deg2Rad * angleB) * radius, 0);
+            Debug.DrawLine(pointA, pointB, color, duration);
         }
 
         // XZ plane
@@ -33,9 +23,9 @@ public class DebugUtils : MonoBehaviour
             float angleA = angleStep * i;
             float angleB = angleStep * (i + 1);
 
-            Vector3 pointA = position + new Vector3(Mathf.Cos(Mathf.Deg2Rad * angleA) * radius, 0, Mathf.Sin(Mathf.Deg2Rad * angleA) * radius);
-            Vector3 pointB = position + new Vector3(Mathf.Cos(Mathf.Deg2Rad * angleB) * radius, 0, Mathf.Sin(Mathf.Deg2Rad * angleB) * radius);
-            Debug.DrawLine(pointA, pointB, color);
+            Vector3 pointA = center + new Vector3(Mathf.Cos(Mathf.Deg2Rad * angleA) * radius, 0, Mathf.Sin(Mathf.Deg2Rad * angleA) * radius);
+            Vector3 pointB = center + new Vector3(Mathf.Cos(Mathf.Deg2Rad * angleB) * radius, 0, Mathf.Sin(Mathf.Deg2Rad * angleB) * radius);
+            Debug.DrawLine(pointA, pointB, color, duration);
         }
 
         // YZ plane
@@ -44,9 +34,9 @@ public class DebugUtils : MonoBehaviour
             float angleA = angleStep * i;
             float angleB = angleStep * (i + 1);
 
-            Vector3 pointA = position + new Vector3(0, Mathf.Cos(Mathf.Deg2Rad * angleA) * radius, Mathf.Sin(Mathf.Deg2Rad * angleA) * radius);
-            Vector3 pointB = position + new Vector3(0, Mathf.Cos(Mathf.Deg2Rad * angleB) * radius, Mathf.Sin(Mathf.Deg2Rad * angleB) * radius);
-            Debug.DrawLine(pointA, pointB, color);
+            Vector3 pointA = center + new Vector3(0, Mathf.Cos(Mathf.Deg2Rad * angleA) * radius, Mathf.Sin(Mathf.Deg2Rad * angleA) * radius);
+            Vector3 pointB = center + new Vector3(0, Mathf.Cos(Mathf.Deg2Rad * angleB) * radius, Mathf.Sin(Mathf.Deg2Rad * angleB) * radius);
+            Debug.DrawLine(pointA, pointB, color, duration);
         }
     }
 }
