e68f3f22b3552ad46237c75d5d74e3b79f735362
Alex <109491531+ilexl@users.noreply.github.com>
2025-06-03 15:01:45 +1200

    Added a main menu script

---
+++ Tank Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    bool searching = false;
+    public void StartMatchMaking()
+    {
+        searching = true;
+        StartCoroutine(TryConnectToServer("dev.legner.foo", 7777));
+    }
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        searching = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private System.Collections.IEnumerator TryConnectToServer(string ip, int port)
+    {
+        while (searching)
+        {
+            var task = URLToIP.IsPortOpenAsync(ip, port);
+            yield return new WaitUntil(() => task.IsCompleted);
+
+            if (task.Result)
+            {
+                Debug.Log($"Port {port} on {ip} is open. Proceeding to connect...");
+                searching = false;
+
+                SceneManager.LoadScene("Dev");
+
+                break;
+            }
+            else
+            {
+                Debug.Log($"Port {port} on {ip} is closed. Retrying in 2 seconds...");
+                yield return new WaitForSeconds(2f);
+            }
+        }
+    }
+
+    
+}
