c90719cc5c5d8f13ad5a2e733625385bf0c5da04
Alex <109491531+ilexl@users.noreply.github.com>
2025-06-08 15:33:56 +1200

    Server now forced to start on DEV.unity

---
+++ Tank Game/Assets/Scripts/ChangeSceneForServer.cs
@@ -0,0 +1,12 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneForServer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(Camera.main == null) { SceneManager.LoadScene("Dev"); }   
+    }
+}
