b9a31ba740a9d9591a3e1fa19d8eed60ff0e13c4
Alex <109491531+ilexl@users.noreply.github.com>
2025-06-10 11:53:54 +1200

    server to simulate

--- Tank Game/Assets/Scripts/TankCombat.cs
+++ Tank Game/Assets/Scripts/TankCombat.cs
@@ -109,7 +109,6 @@ public class TankCombat : NetworkBehaviour
     [ClientRpc]
     private void BroadcastShotClientRpc(ulong shooterClientId, long seed, Vector3 pos, Vector3 dir, int projectile)
     {
-        if (IsServer) { return; } // DO NOT SPAWN PROJECTILE ON THE SERVER
         ProjectileDefinition projectileDefinition = ProjectileDatabase.GetProjectile(ProjectileKey.T99APT);
         Projectile.Create(pos, dir, seed, projectileDefinition);
     }
